cab9a56157fc2ca16ef0422a841010ee05482a2f
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-05-19 08:23:29 -0300

    Criando model para consultas ao banco de dados

---
+++ src/models/giftList.model.js
@@ -0,0 +1,15 @@
+const giftListModel = (sequelize, DataTypes) => {
+    const giftList = sequelize.define('giftList', {
+        idGift: {type: DataTypes.INTEGER, primaryKey: true}, 
+        giftName: DataTypes.STRING,  
+        selected: DataTypes.BOOLEAN, 
+        idImage: DataTypes.INTEGER, 
+        giftGiver: DataTypes.STRING
+    }, {
+        timestamps: false
+    })
+
+    return giftList
+}
+
+module.exports = giftListModel
